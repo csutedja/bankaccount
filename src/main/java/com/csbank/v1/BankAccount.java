0ec1c98e3ac555fab1ee6b0124fb09dfaf04dc86
Chris Sutedja <christopher.sutedja@gmail.com>
2024-04-13 18:34:08 -0400

    Fixing withdrawal test and merge conflicts.

--- src/main/java/com/csbank/v1/BankAccount.java
+++ src/main/java/com/csbank/v1/BankAccount.java
@@ -11,52 +11,23 @@ public class BankAccount {
         return balance;
     }
 
-<<<<<<< Updated upstream
-    public boolean deposit(int deposit) {
-        if (deposit > 0){
-            balance = balance + deposit;
-=======
     public boolean deposit(int amount) {
-        if (amount > 0){
-            balance = balance + amount;
->>>>>>> Stashed changes
-        } else {
-            System.out.println("Invalid input.");
-            return false;
-        }
-        return true;
-    }
-
-<<<<<<< Updated upstream
-    public boolean withdraw(int withdraw) {
-        if (withdraw <= 0) {
-            System.out.println("Invalid input.");
-            return false;
-        } else if (balance - withdraw < 0) {
-            System.out.println("Insufficient funds.");
-            return false;
-        } else {
-            balance -= withdraw;
+        if (amount > 0) {
+            balance += amount;
             return true;
+        } else {
+            System.out.println("Invalid input.");
+            return false;
         }
     }
 
-}
-    // run the test, see the compilation error, and construct the class
-    // based on the requirements stated in the test.
-
-
-
-
-
-
-
-=======
     public int withdraw(int amount) {
         if (amount <= 0) {
             System.out.println("Invalid input.");
+
         } else if (amount > balance) {
             System.out.println("Insufficient funds.");
+
         } else {
             balance -= amount;
             return amount;
@@ -64,4 +35,3 @@ public class BankAccount {
         return amount;
     }
 }
->>>>>>> Stashed changes
--- src/test/java/com/csbank/v1/BankAccountTest.java
+++ src/test/java/com/csbank/v1/BankAccountTest.java
@@ -25,7 +25,8 @@ public class BankAccountTest {
     @Test
     public void testSuccessfulWithdrawal() {
         BankAccount account = new BankAccount(1000);
-        assertTrue(account.withdraw(100));
+        int amount = account.withdraw(100);
+        assertEquals(100, amount);
         assertEquals(900, account.getBalance());
     }
 
@@ -39,26 +40,14 @@ public class BankAccountTest {
     @Test
     public void testNegativeWithdrawal() {
         BankAccount account = new BankAccount(1000);
-<<<<<<< Updated upstream
-        assertFalse(account.withdraw(-10));
-=======
         assertEquals(-10, account.withdraw(-10));
->>>>>>> Stashed changes
         assertEquals(1000, account.getBalance());
     }
 
     @Test
     public void testOverWithdrawal() {
         BankAccount account = new BankAccount(1000);
-<<<<<<< Updated upstream
-        assertFalse(account.withdraw(10000));
-        assertEquals(1000, account.getBalance());
-    }
-    // write the negative case.  cases that break your class!
-=======
         assertEquals(10000, account.withdraw(10000));
         assertEquals(1000, account.getBalance());
     }
-
->>>>>>> Stashed changes
 }
